113c37b4fc48923937003a3c0e06c4f9a7ccba6e
pjsingal <pjs2206@columbia.edu>
2025-06-23 20:15:07 +0000

    Added getPES function

---
+++ test/runLMRRfactory.py
@@ -0,0 +1,27 @@
+import LMRRfactory
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--date', type=str)
+parser.add_argument('--allPdep', type=str, default='False')
+parser.add_argument('--allPLOG', type=str, default='False')
+args = parser.parse_args()
+
+allPdep = 'True'
+allPLOG = 'True'
+date = 'May07'
+
+models = {
+    'AramcoMech30': 'test/data/aramco30.yaml',
+    }
+
+for m in models.keys():
+    LMRRfactory.makeYAML(mechInput=models[m],
+                        outputPath=f"test/outputs/{date}")
+    if allPdep == 'True':
+        LMRRfactory.makeYAML(mechInput=models[m],
+                            outputPath=f"test/outputs/{date}",
+                            allPdep=True)
+    if allPLOG == 'True':
+        LMRRfactory.makeYAML(mechInput=models[m],
+                            outputPath=f"test/outputs/{date}",
+                            allPLOG=True)
