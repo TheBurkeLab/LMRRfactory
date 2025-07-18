fecd4864eb56b75db898d19f9a2b3a6de92f51fb
pjsingal <pjs2206@columbia.edu>
2025-07-18 17:15:05 +0000

    Fixing yaml dump issues

--- src/LMRRfactory/generate.py
+++ src/LMRRfactory/generate.py
@@ -432,9 +432,9 @@ class makeYAML:
                 #                 print(f"    subkey: {sk}, type: {type(sv)}")
                 yaml_str = yaml.dump(newRxn)
                 # print("0")
-                # print(yaml_str)
+                print(yaml_str)
                 newRxn_obj = ct.Reaction.from_yaml(yaml_str,data['mech_obj'])
-                print(newRxn_obj)
+                
                 newReactions.append(newRxn_obj)
                 # print("finished")
                 print(f"{mech_rxn} {dict(data['mech_pes'][i])} converted to LMR-R with ab initio parameters")
