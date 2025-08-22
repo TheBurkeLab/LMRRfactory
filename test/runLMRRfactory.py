6ec42beb9ec7556e59490d397ab1dd74b6fbaa90
pjsingal <pjs2206@columbia.edu>
2025-08-22 16:37:12 +0000

    Fixing dbase params
    Composition matching for col species
    
    Fixing dbase params

--- test/runLMRRfactory.py
+++ test/runLMRRfactory.py
@@ -10,9 +10,9 @@ allPLOG = 'True'
 date = 'July29'
 
 models = {
-    # 'AramcoMech30': 'test/data/aramco30.yaml',
-    'Klippenstein-2018': 'test/data/klippenstein-CNF2018-original.yaml'
-    # 'Gutierrez-2025': 'test/data/gutierrez-2025.yaml'
+    'AramcoMech30': 'test/data/aramco30.yaml',
+    'Klippenstein-2018': 'test/data/klippenstein-CNF2018-original.yaml',
+    'Gutierrez-2025': 'test/data/gutierrez-2025.yaml',
     }
 
 for m in models.keys():
