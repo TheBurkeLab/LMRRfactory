1f4f8b67873c323c207476a4a0b96e3e6e15bf67
pjsingal <pjs2206@columbia.edu>
2026-02-10 00:29:22 +0000

    normalizing user rxn input

--- test/runLMRRfactory.py
+++ test/runLMRRfactory.py
@@ -12,10 +12,11 @@ date = 'Feb09'
 models = {
     # 'testmech': 'test/data/test_mech.yaml',
     # 'AramcoMech30': 'test/data/aramco30.yaml',
-    'Klippenstein-2018': 'test/data/klippenstein-CNF2018-original.yaml',
+    # 'Klippenstein-2018': 'test/data/klippenstein-CNF2018-original.yaml',
     # 'Alzueta': 'test/data/alzuetamechanism.yaml',
     # 'Gutierrez-2025': 'test/data/gutierrez-2025.yaml',
     # 'kane-h2nno': 'test/data/glarborg_H2NNO_B.yaml',
+    'kane-h2nno_uni': 'test/data/glarborg_H2NNO_B_unidirectionalexample.yaml',
     # # 'meng-h2nno2': 'test/data/glarborg_H2NNO2_B.yaml'
     }
 
@@ -44,16 +45,20 @@ for m in models.keys():
         #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='NH2 + H (+AR) <=> NH3 (+AR)')
         # LMRRfactory.makeYAML(mechInput=models[m],
         #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='HOCO (+M) <=> CO2 + H (+M)')
+        # LMRRfactory.makeYAML(mechInput=models[m],
+        #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2+H(+M)=>C2H3(+M)')
+        # LMRRfactory.makeYAML(mechInput=models[m],
+        #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='C$_2$H$_2$+H(+M)=C$_2$H$_3$(+M)')
+        # LMRRfactory.makeYAML(mechInput=models[m],
+        #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2 + H (+M) = C2H3 ( + M)')
+        # LMRRfactory.makeYAML(mechInput=models[m],
+        #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2+H(+  M)=C2H3(+M)')
+        # LMRRfactory.makeYAML(mechInput=models[m],
+        #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2 +H( + M)=C2H3(+M)')
         LMRRfactory.makeYAML(mechInput=models[m],
-                            outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2+H(+M)=>C2H3(+M)')
-        LMRRfactory.makeYAML(mechInput=models[m],
-                            outputPath=f"test/outputs/{date}",allPdep=True,reaction='C$_2$H$_2$+H(+M)=C$_2$H$_3$(+M)')
-        LMRRfactory.makeYAML(mechInput=models[m],
-                            outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2 + H (+M) = C2H3 ( + M)')
-        LMRRfactory.makeYAML(mechInput=models[m],
-                            outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2+H(+  M)=C2H3(+M)')
-        LMRRfactory.makeYAML(mechInput=models[m],
-                            outputPath=f"test/outputs/{date}",allPdep=True,reaction='C2H2 +H( + M)=C2H3(+M)')
+                            outputPath=f"test/outputs/{date}",allPdep=True,reaction='H2O$$$+N2 =>tNHNOH')
+        # LMRRfactory.makeYAML(mechInput=models[m],
+        #                     outputPath=f"test/outputs/{date}",allPdep=True,reaction='NH2NO <= tNHNOH')
         # LMRRfactory.makeYAML(mechInput=models[m],
         #                 outputPath=f"test/outputs/{date}",allPdep=True,reaction='H + O2 (+M) <=> HO2 (+M)') 
     # LMRRfactory.makeYAML(mechInput=models[m],
